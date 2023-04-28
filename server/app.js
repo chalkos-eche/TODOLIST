93dd7975f0704f2be6307b8ffc9b8624bc001495
smurf <hm@HMui-MacBookPro.local>
2023-04-28 16:26:09 +0900

    project : 기본서버

--- server/app.js
+++ server/app.js
@@ -1,40 +1,13 @@
-const express = require("express");
-// const multer = require("multer");
-// const path = require("path");
+const express = require('express');
 const app = express();
-const PORT = 8080;
+const port = 8000;
 
-// app.set("view engine", "ejs");
-// app.use("/views", express.static(__dirname + "/views"));
-app.use("/static", express.static(__dirname + "/static"));
-// app.use("/uploads", express.static(`${__dirname}/uploads`));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// const uploadDetail = multer({
-//   storage: multer.diskStorage({
-//     destination(req, file, done) {
-//       //destination:경로설정
-//       // done :callback 함수
-//       done(null, "uploads/");
-//     },
-//     filename(req, file, done) {
-//       const ext = path.extname(file.originalname);
-//       done(null, path.basename(req.body.id, ext) + ext);
-//       //  [파일명 +현재시간.확장자] 형식으로 업로드
-//     },
-//   }),
-// });
-// const indexRouter = require("./routes");
-// app.use("/", indexRouter);
+const todoRouter = require('./routes/todo');
+app.use('/api', todoRouter); // 기본주소: localhost:PORT/api
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
-app.get("*", (req, res) => {
-  // res.send('404 Error! 잘못된 주소 형식입니다.');
-  res.render("404");
-});
-app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
 });
